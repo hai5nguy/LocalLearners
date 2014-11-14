2ca03382d9fc50bf1a95b0293499ea4dd219b774
hnguyen <hnguyen@exacttarget.com>
2014-11-14 12:37:05 -0500

    woot ngview working, paging working

--- app/public/main.js
+++ app/public/main.js
@@ -1,10 +1,8 @@
-var localLearnerApp = angular.module('localLearnerApp',['ngRoute']);
-
-//
-//    .config(function($routeProvider) {
-//        $routeProvider.when('/login',
-//            {
-//                templateUrl: 'templates/login.html',
-//                controller: 'LoginController'
-//            })
-//    });
+var localLearnerApp = angular.module('localLearnerApp',['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/',
+            {
+                templateUrl: 'components/main/main.html',
+                controller: 'MainCtrl'
+            })
+    });
