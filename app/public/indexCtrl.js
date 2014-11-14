f4aaa670270de96a640841e1859e9e5caa2f9ace
hnguyen <hnguyen@exacttarget.com>
2014-11-14 14:23:32 -0500

    refactor appSettings url object

--- app/public/indexCtrl.js
+++ app/public/indexCtrl.js
@@ -1,7 +1,8 @@
 localLearnerApp.controller('IndexCtrl', function($scope) {
     $scope.appSettings = {
-        userProfileTemplateUrl: 'templates/userprofile.html',
-        headerUrl: 'components/header/header.html',
-        footerUrl: 'components/footer/footer.html'
+        url: {
+            header: 'components/header/header.html',
+            footer: 'components/footer/footer.html'
+        }
     }
 });
