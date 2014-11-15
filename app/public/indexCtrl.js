4ff815db5c4af10d389b37fcd460ad56c7900d6c
hnguyen <hnguyen@exacttarget.com>
2014-11-15 15:41:44 -0500

    added include-replace directive

--- app/public/indexCtrl.js
+++ app/public/indexCtrl.js
@@ -1,8 +1,18 @@
-localLearnerApp.controller('IndexCtrl', function($scope) {
-    $scope.appSettings = {
-        url: {
-            header: 'components/header/header.html',
-            footer: 'components/footer/footer.html'
+localLearnerApp
+    .controller('IndexCtrl', function($scope) {
+        $scope.appSettings = {
+            url: {
+                header: 'components/header/header.html',
+                footer: 'components/footer/footer.html'
+            }
         }
-    }
-});
+    })
+    .directive('includeReplace', function () {
+        return {
+            require: 'ngInclude',
+            restrict: 'A', /* optional */
+            link: function (scope, el, attrs) {
+                el.replaceWith(el.children());
+            }
+        };
+    });
