dcee0df1fc5e3e5301cef80ac7c736803d09ffd7
hnguyen <hnguyen@exacttarget.com>
2014-11-29 14:46:10 -0500

    corrected port for heroku

--- app/webserver.js
+++ app/webserver.js
@@ -4,7 +4,7 @@ var app = express();
 //var app = require('http').createServer(app);
 //var io = require('socket.io')(server);
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 
 app.listen(port, function() {
