cadb08702a6808a4f4ee3a8449d0f0f65c3c9bc8
hnguyen <hnguyen@exacttarget.com>
2014-12-08 11:31:43 -0500

    more passport fix

--- app/webserver.js
+++ app/webserver.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var passport = require('passport');
 var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var session = require('express-session');
 
 //var session = require('express-session');
 
@@ -15,6 +16,23 @@ var MEETUP_KEY = process.env.MEETUP_KEY || 'h0dl8qkd82gbjan5cpr8plb4jq';
 var MEETUP_SECRET = process.env.MEETUP_SECRET || 'seagvb265dc9j1vm53q9pvu9r8';
 
 
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+
+app.use(session({
+    secret: 'blah',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+
 passport.use('provider', new OAuth2Strategy({
     authorizationURL: 'https://secure.meetup.com/oauth2/authorize',
     tokenURL: 'https://secure.meetup.com/oauth2/access',
@@ -27,6 +45,7 @@ passport.use('provider', new OAuth2Strategy({
         console.log('refreshToken ', refreshToken);
         console.log('profile ', profile);
         console.log('done ', done);
+        return done(null, { name: 'blah'}, {});
     })
 );
 
@@ -65,7 +84,7 @@ app.get('/authenticate/callback',
 
 
 //app.use('/test', express.static(__dirname + '/public/test.html?' + process.env.testkey + '=' + process.env.testvalue));
-app.use('/', express.static(__dirname + '/public/index.html'));
+//app.use('/', express.static(__dirname + '/public/index.html'));
 app.use(express.static(__dirname + '/public'));
 //
 //app.use(session({
@@ -76,8 +95,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
-//app.use(passport.initialize());
-//app.use(session());
+
 
 app.listen(app.get('port'), function() {
     console.log("Web server running on http://localhost:" + app.get('port'));
