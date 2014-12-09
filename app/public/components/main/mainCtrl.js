09583ffcc1f2ef0eed99fcd3dc79ab06ae0d989e
alissa <alissa.renz@gmail.com>
2014-12-09 16:28:19 -0500

    Set up MainCtrl to pull class data
    MainCtrl now pulls in the ’Upcoming Class’ & ‘Most Requested’ sections’
    data; suggest creating separate ClassCtrl since same functionality will
    be used for pages other than Main

--- app/public/components/main/mainCtrl.js
+++ app/public/components/main/mainCtrl.js
@@ -1,3 +1,31 @@
-localLearnerApp.controller('MainCtrl', function() {
+localLearnerApp.controller('MainCtrl', function($scope) {
+
+	$scope.availClasses = [
+        {name:'Intro to Guitar',date:'December 18, 2015',location:'Downtown',category:'music',image:'music.png'},
+        {name:'Advanced CSS Techniques',date:'October 5, 2015',location:'Fishers',category:'technology',image:'technology.png'},
+        {name:'Basketweaving',date:'March 1, 2015',location:'Carmel',category:'DIY',image:'diy.png'},
+        {name:'Ultimate Frisbee 101',date:'August 4, 2015',location:'Downtown',category:'sports',image:'sports.png'},
+        {name:'Cooking Steaks',date:'January 21, 2015',location:'Fountain Square',category:'culinary',image:'culinary.png'},
+        {name:'Broadway for Dummies',date:'September 21, 2015',location:'Downtown',category:'performing-arts',image:'performing-arts.png'},
+        {name:'Taking Care of Cats',date:'June 28, 2015',location:'Avon',category:'other',image:'other.png'}
+        ];
+
+    $scope.reqClasses = [
+        {name:'Intro to Photoshop',category:'technology',requests:5},
+        {name:'Intermediate Guitar',category:'music',requests:34},
+        {name:'Using a Pottery Wheel',category:'visual-arts',requests:53},
+        {name:'Advanced Bootstrap 3',category:'technology',requests:42},
+        {name:'Changing Your Breakpads',category:'diy',requests:9},
+        {name:'Retirement and 401K',category:'business',requests:1},
+        {name:'Advanced Pillowforts',category:'other',requests:10},
+        {name:'Designing for the Blind',category:'visual-arts',requests:26},
+        {name:'Roth IRAs',category:'business',requests:17},
+        {name:'Intro to Bird Watching',category:'other',requests:4},
+        {name:'Film Appreciation',category:'visual-arts',requests:7},
+        {name:'Tornado Chasing',category:'other',requests:0},
+        {name:'Archery',category:'sports',requests:4},
+        {name:'Special Effects',category:'technology',requests:3},
+        {name:'Planning a Birthday Party',category:'other',requests:15},
+    ];
 
 });
