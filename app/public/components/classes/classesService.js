ccb9790567e5573927c51edd485dd73506ba9126
alissa <alissa.renz@gmail.com>
2014-12-16 15:23:52 -0500

    Augmented Dummy Classes; Tweaked Category Filter

--- app/public/components/classes/classesService.js
+++ app/public/components/classes/classesService.js
@@ -13,7 +13,16 @@ localLearnerApp.factory('ClassesService', function () {
                 {name:'Ultimate Frisbee 101',date:'August 4, 2015',location:'Downtown',category:'sports',image:'sports.png'},
                 {name:'Cooking Steaks',date:'January 21, 2015',location:'Fountain Square',category:'culinary',image:'culinary.png'},
                 {name:'Broadway for Dummies',date:'September 21, 2015',location:'Downtown',category:'performing-arts',image:'performing-arts.png'},
-                {name:'Taking Care of Cats',date:'June 28, 2015',location:'Avon',category:'other',image:'other.png'}
+                {name:'Taking Care of Cats',date:'June 28, 2015',location:'Avon',category:'other',image:'other.png'},
+                {name:'Intro to Piano',date:'August 5, 2015',location:'Lawrence',category:'music',image:'music.png'},
+                {name:'Water Aerobics',date:'October 7, 2015',location:'Fountain Square',category:'fitness',image:'fitness.png'},
+                {name:'Fix Your Break Pads',date:'November 6, 2015',location:'Greenwood',category:'automotive',image:'automotive.png'},
+                {name:'Appreciate Art Better',date:'February 20, 2015',location:'Fishers',category:'visual-arts',image:'visual-arts.png'},
+                {name:'Spanish',date:'January 5, 2015',location:'Carmel',category:'language',image:'language.png'},
+                {name:'French',date:'January 5, 2015',location:'Carmel',category:'language',image:'language.png'},
+                {name:'German',date:'January 5, 2015',location:'Carmel',category:'language',image:'language.png'},
+                {name:'Swedish',date:'January 5, 2015',location:'Carmel',category:'language',image:'language.png'},
+                {name:'English',date:'January 5, 2015',location:'Carmel',category:'language',image:'language.png'}
             ];
 
             var filter = settings.category === '' ? {} : { category: settings.category };
