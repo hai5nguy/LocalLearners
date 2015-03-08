9b7f37a135ccd6c84ea8d3532ffd83ca0e40bbec
hnguyen <hnguyen@exacttarget.com>
2015-03-08 10:43:53 -0400

    hai: added requested classes to main search

--- app/public/components/search/mainsearch/mainSearchDirective.js
+++ app/public/components/search/mainsearch/mainSearchDirective.js
@@ -17,15 +17,81 @@ localLearnersApp.directive('llMainSearch', function() {
             });
 
             function updateSearchDate() {
-                ClassesService.getUpcomingClasses().then(function(upcomingClasses) {
-                    $scope.upcomingClasses = upcomingClasses;
-                }, function (err) {
-                    console.log('mainsearchtempate error: ', err);
-                });
-            }
+                
+                //var searchData = [];
+                //
+                //ClassesService.getUpcomingClasses().then(function (upcomingClasses) {
+                //
+                //    var trimmedUpcomingClasses = _.map(upcomingClasses, function (u) {
+                //        return {
+                //            type: 'upcomingClass',
+                //            id: u.eventId, 
+                //            name: u.name
+                //        }
+                //    });
+                //    
+                //    searchData.push(trimmedUpcomingClasses);
+                //    
+                //    ClassesService.getRequestedClasses().then(function (requestedClasses) {
+                //        var trimmedRequestedClasses = _.map(requestedClasses, function (r) {
+                //            return {
+                //                type: 'requestedClass',
+                //                name: r.name,
+                //                id: r._id
+                //            }
+                //        });
+                //        
+                //        searchData.push(trimmedRequestedClasses);
+                //        
+                //        console.log('11 ', searchData);
+                //        $scope.searchData = searchData;
+                //    }, function (err) {
+                //        console.log('mainsearch error getting requested classes: ', err);
+                //    });
+                //}, function (err) {
+                //    console.log('mainsearch error getting upcoming classes: ', err);
+                //});
+                //
 
+
+                //var searchData = [];
+                
+                ClassesService.getUpcomingClasses().then(function (upcomingClasses) {
+                
+                    var trimmedUpcomingClasses = _.map(upcomingClasses, function (u) {
+                        return {
+                            type: 'Upcoming',
+                            id: u.eventId,
+                            name: u.name
+                        }
+                    });
+                    
+                    $scope.searchData = _.union($scope.searchData, trimmedUpcomingClasses);
+                }, function (err) {
+                    console.log('mainsearch error getting upcoming classes: ', err);
+                });
+
+                ClassesService.getRequestedClasses().then(function (requestedClasses) {
+                    var trimmedRequestedClasses = _.map(requestedClasses, function (r) {
+                        return {
+                            type: 'Requested',
+                            name: r.name,
+                            id: r._id
+                        }
+                    });
+
+                    //searchData.push(trimmedRequestedClasses);
+
+                    //console.log('11 ', searchData);
+
+                    $scope.searchData = _.union($scope.searchData, trimmedRequestedClasses);
+
+                    //$scope.searchData = searchData;
+                }, function (err) {
+                    console.log('mainsearch error getting requested classes: ', err);
+                });
+                
+            }
         }
     }
-
-
 });
