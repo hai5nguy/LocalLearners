2bcc022679d4da4602791e2956350cd48618a745
hnguyen <hnguyen@exacttarget.com>
2015-07-26 16:27:05 -0400

    clean up

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,12 +1,10 @@
 var Q           = require(LL_NODE_MODULES_DIR + 'q');
 
 var Category            = require(LL_MODULES_DIR + 'Category.js');
-//var CategoryCollection  = require(LL_MODULES_DIR + 'CategoryCollection.js');
 
 module.exports = (function () {
     THE_APP.get('/api/category/all', function (req, res) {
 
-        
         var category = Category({
             req: req
         });
@@ -17,15 +15,5 @@ module.exports = (function () {
             res.status(500).send(category.error);
         });
 
-
-
-
-
-        //var context = new CONTEXT();
-        //Category.getAll(context)().then(function() {
-        //    res.json(context.Category.all);
-        //}, function () {
-        //    res.status(500).send(context.Error);
-        //});
     });
 })();
